d59305122fe74804f22e4ca4e1c9d0a7b301863b
rcph <rcph110110@gmail.com>
2022-12-13 22:11:39 +0800

    looksteribblexd

--- script/app.js
+++ script/app.js
@@ -1,18 +1,64 @@
 class Board {
     constructor() {
+        this.board_wrapper = document.querySelector(".board_wrapper")
+        this.pieces_wrapper = document.querySelector(".pieces_wrapper")
         this.squares = this.initialize_squares()
         this.initialize_style()
+
+        // white true, false black
+        this.piece_turn = 1
+        this.movable_piece = null
+        this.movable_active = false
+        this.active_tile = null
+
+        this.squares.forEach((square, iSquare) => {
+            square.addEventListener("click", (e) => {
+
+                // if(this.piece_turn == square.children[0].getAttribute("data-piece")[0] - 1) console.log("not same")
+
+                if (square.children.length == 1 && this.movable_piece != square.children[0] && !this.movable_active) {
+                    this.movable_active = true
+                    this.movable_piece = square.children[0]
+                    this.active_tile = iSquare
+                    square.style.backgroundColor = "lightgreen"
+                }
+                else if (this.movable_piece == square.children[0]) {
+
+                    this.squares[this.active_tile].style.backgroundColor = this.squares[this.active_tile].getAttribute("data-color")
+                    this.movable_active = false
+                    this.movable_piece = null
+                }
+                else if (square.children.length == 1) {
+
+                    this.squares[this.active_tile].style.backgroundColor = this.squares[this.active_tile].getAttribute("data-color")
+                    this.removePiece(square.children[0])
+                    this.removePiece(this.movable_piece)
+                    this.appendPiece(this.movable_piece, iSquare)
+                    this.movable_active = false
+                    this.movable_piece = null
+                    // this.piece_turn = !this.piece_turn
+                }
+                else if (square.children.length != 1) {
+                    this.squares[this.active_tile].style.backgroundColor = this.squares[this.active_tile].getAttribute("data-color")
+                    this.removePiece(this.movable_piece)
+                    this.appendPiece(this.movable_piece, iSquare)
+                    this.movable_active = false
+                    this.movable_piece = null
+                    // this.piece_turn = !this.piece_turn
+                }
+            })
+        })
     }
     initialize_squares() {
-        const board_wrapper = document.querySelector(".board_wrapper")
         for (let iSquare = 0; iSquare < 64; ++iSquare) {
             const square = document.createElement("div")
             square.setAttribute("class", "square")
             square.setAttribute("data-iSquare", iSquare)
-            square.addEventListener("pointerdown", (e) => {
-                dot(e.target.getBoundingClientRect().left + (e.target.offsetWidth / 2), e.target.getBoundingClientRect().top + (e.target.offsetHeight / 2))
-            })
-            board_wrapper.appendChild(square)
+            // square.addEventListener("click", (e) => {
+            //     const coordinate_x = e.target.getBoundingClientRect().left + (e.target.offsetWidth / 2)
+            //     const coordinate_y =  e.target.getBoundingClientRect().top + (e.target.offsetHeight / 2)
+            // })
+            this.board_wrapper.appendChild(square)
         }
         return Array.from(document.querySelectorAll(".square"))
     }
@@ -21,8 +67,14 @@ class Board {
         this.squares.forEach((square, sIndex) => {
             if (sIndex % 8 == 0) shift = !shift
 
-            if ((sIndex + shift) % 2 == 0) square.style.backgroundColor = "#eeeed2"
-            else square.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+            if ((sIndex + shift) % 2 == 0) {
+                square.style.backgroundColor = "#eeeed2"
+                square.setAttribute("data-color", "#eeeed2")
+            }
+            else {
+                square.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+                square.setAttribute("data-color", "rgba(0, 0, 0, 0.5)")
+            }
         })
     }
     initialize_matrices(matrices) {
@@ -36,8 +88,11 @@ class Board {
         })
     }
     appendPiece(piece, index) {
-        if (this.squares[index].children.length > 0) this.squares[index].children[0].remove()
+        if (this.squares[index].children.length > 0) return
         this.squares[index].appendChild(piece)
+    }
+    removePiece(piece, index) {
+        if (piece) piece.remove()
     }
 }
 class Pieces {
@@ -61,6 +116,7 @@ class Pieces {
     choose(empire, iPieces) {
         const piece = document.createElement("img")
         piece.setAttribute("class", "piece")
+        piece.setAttribute("data-piece", `${empire}${iPieces}`)
         piece.src = `${this.directory}/${this.empire[empire]}${this.pieces[iPieces].name}.${this.file_extension}`
 
         return piece
